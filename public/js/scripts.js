b35347a7bfe83d9b03baa06f259de3fb0e05b1f3
KonkukCodeCat <mr8356@naver.com>
2022-06-22 04:19:50 +0900

    Add front code

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,13 +5,51 @@ const chatting_box = $("#chatting_box");
 const form = $("#chat_form");
 
 // global socket handler
-socket.on('user_connected' , (username)=>{
-    console.log(`user ${username} connected!`);
-})
+//* global socket handler
+socket.on('user_connected', (username) => {
+    drawNewChat(`${username} connected!`);
+});
 
-function drawhelloStranger(username){
-    hello_stranger.text(`Hello ${username} Stranger`);
-}
+socket.on('new_chat', (data) => {
+    const { chat, username } = data;
+    drawNewChat(`${username}: ${chat}`);
+});
+socket.on('disconnect_user', (username) => drawNewChat(`${username}: bye...`));
+
+//* event callback functions
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const inputValue = event.target.elements[0].value;
+    if (inputValue !== '') {
+    socket.emit('submit_chat', inputValue);
+    // 화면에다가 그리기
+    drawNewChat(`me : ${inputValue}`, true);
+    event.target.elements[0].value = '';
+    }
+};
+
+const drawHelloStranger = (username) =>
+    (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+const drawNewChat = (message, isMe = false) => {
+    const wrapperChatBox = document.createElement('div');
+    wrapperChatBox.className = 'clearfix';
+    let chatBox;
+    if (!isMe)
+        chatBox = `
+        <div class='bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix break-all'>
+        ${message}
+        </div>
+        `;
+    else
+        chatBox = `
+        <div class='bg-white w-3/4 ml-auto mr-4 my-2 p-2 rounded-lg clearfix break-all'>
+        ${message}
+        </div>
+        `;
+    wrapperChatBox.innerHTML = chatBox;
+    chattingBoxElement.append(wrapperChatBox);
+};
+
 
 function helloUser() {
     const username = prompt('What is your name?');
