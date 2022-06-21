b25821506d76be186e16e2de33cc1558d05694cd
KonkukCodeCat <mr8356@naver.com>
2022-06-22 03:59:46 +0900

    Add brocast

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,6 +4,15 @@ const hello_stranger = $("#hello_stranger");
 const chatting_box = $("#chatting_box");
 const form = $("#chat_form");
 
+// global socket handler
+socket.on('user_connected' , (username)=>{
+    console.log(`user ${username} connected!`);
+})
+
+function drawhelloStranger(username){
+    hello_stranger.text(`Hello ${username} Stranger`);
+}
+
 function helloUser() {
     const username = prompt('What is your name?');
     socket.emit('new_user' , {
@@ -12,6 +21,7 @@ function helloUser() {
     socket.on('hello_user' , (data)=>{
         console.log(data);
     })
+    drawhelloStranger(username);
 }
 
 function init() {
--- src/chats.gateway.ts
+++ src/chats.gateway.ts
@@ -1,16 +1,16 @@
 import { Logger } from '@nestjs/common';
-import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'dgram';
+import {  ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Socket } from 'socket.io' /// 중요!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 @WebSocketGateway({namespace : "log"})
 export class ChatsGateway implements OnGatewayConnection , OnGatewayDisconnect{
 
   private logger = new Logger('chat');
   handleConnection(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`connected : ${socket.address}`)
+    this.logger.log(`connected : ${socket.id}`)
   }
   handleDisconnect(@ConnectedSocket() socket: Socket) {
-    this.logger.log(`disconnected : ${socket.address}`)
+    this.logger.log(`disconnected : ${socket.id}`)
   }
 
   @SubscribeMessage('new_user')
@@ -18,5 +18,6 @@ export class ChatsGateway implements OnGatewayConnection , OnGatewayDisconnect{
     const username = body.username;
     console.log(username);
     socket.emit("hello_user" , "hello " + username);
+    socket.broadcast.emit('user_connected' , username);
   }
 }
