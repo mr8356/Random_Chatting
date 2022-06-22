f6dd8db08a25f82d7157a3c346fa56f01426f20a
KonkukCodeCat <mr8356@naver.com>
2022-06-22 16:59:05 +0900

    Add chatt

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,69 +1,68 @@
-const socket = io('/log');
+const socket = io('/chattings');
 
-const hello_stranger = $("#hello_stranger");
-const chatting_box = $("#chatting_box");
-const form = $("#chat_form");
+const getElementById = (id) => document.getElementById(id) || null;
 
-// global socket handler
+//* get DOM element
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const formElement = getElementById('chat_form');
+
 //* global socket handler
 socket.on('user_connected', (username) => {
-    drawNewChat(`${username} connected!`);
+  drawNewChat(`${username} connected!`);
 });
-
 socket.on('new_chat', (data) => {
-    const { chat, username } = data;
-    drawNewChat(`${username}: ${chat}`);
+  const { chat, username } = data;
+  drawNewChat(`${username}: ${chat}`);
 });
 socket.on('disconnect_user', (username) => drawNewChat(`${username}: bye...`));
 
 //* event callback functions
 const handleSubmit = (event) => {
-    event.preventDefault();
-    const inputValue = event.target.elements[0].value;
-    if (inputValue !== '') {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  if (inputValue !== '') {
     socket.emit('submit_chat', inputValue);
     // 화면에다가 그리기
     drawNewChat(`me : ${inputValue}`, true);
     event.target.elements[0].value = '';
-    }
+  }
 };
 
+//* draw functions
 const drawHelloStranger = (username) =>
-    (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+  (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
 const drawNewChat = (message, isMe = false) => {
-    const wrapperChatBox = document.createElement('div');
-    wrapperChatBox.className = 'clearfix';
-    let chatBox;
-    if (!isMe)
-        chatBox = `
-        <div class='bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix break-all'>
-        ${message}
-        </div>
-        `;
-    else
-        chatBox = `
-        <div class='bg-white w-3/4 ml-auto mr-4 my-2 p-2 rounded-lg clearfix break-all'>
-        ${message}
-        </div>
-        `;
-    wrapperChatBox.innerHTML = chatBox;
-    chattingBoxElement.append(wrapperChatBox);
+  const wrapperChatBox = document.createElement('div');
+  wrapperChatBox.className = 'clearfix';
+  let chatBox;
+  if (!isMe)
+    chatBox = `
+    <div class='bg-gray-300 w-3/4 mx-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  else
+    chatBox = `
+    <div class='bg-white w-3/4 ml-auto mr-4 my-2 p-2 rounded-lg clearfix break-all'>
+      ${message}
+    </div>
+    `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.append(wrapperChatBox);
 };
-
 
 function helloUser() {
-    const username = prompt('What is your name?');
-    socket.emit('new_user' , {
-        "username" : username
-    })
-    socket.on('hello_user' , (data)=>{
-        console.log(data);
-    })
-    drawhelloStranger(username);
+  const username = prompt('What is your name?');
+  socket.emit('new_user', username, (data) => {
+    drawHelloStranger(data);
+  });
 }
 
 function init() {
-    helloUser();
+  helloUser();
+  // 이벤트 연결
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
--- src/chats.gateway.ts
+++ src/chats.gateway.ts
@@ -2,22 +2,29 @@ import { Logger } from '@nestjs/common';
 import {  ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { Socket } from 'socket.io' /// 중요!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-@WebSocketGateway({namespace : "log"})
+@WebSocketGateway({namespace : "chattings"})
 export class ChatsGateway implements OnGatewayConnection , OnGatewayDisconnect{
 
   private logger = new Logger('chat');
   handleConnection(@ConnectedSocket() socket: Socket) {
     this.logger.log(`connected : ${socket.id}`)
   }
+  
   handleDisconnect(@ConnectedSocket() socket: Socket) {
     this.logger.log(`disconnected : ${socket.id}`)
   }
 
   @SubscribeMessage('new_user')
-  handleMessage(@MessageBody() body , @ConnectedSocket() socket: Socket) {
-    const username = body.username;
-    console.log(username);
+  handleMessage(@MessageBody() username:string , @ConnectedSocket() socket: Socket) {
     socket.emit("hello_user" , "hello " + username);
     socket.broadcast.emit('user_connected' , username);
+  }
+
+  @SubscribeMessage('submit_chat')
+  submitChat(@MessageBody() message : string , @ConnectedSocket() socket:Socket){
+    socket.broadcast.emit("new_chat" , {
+      'chat' : message,
+      'username' : socket.id
+    });
   }
 }
